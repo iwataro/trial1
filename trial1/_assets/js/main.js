ceae9bd7d5e48c68bc09e149d7a0e3b33ef62baa
iwataro <62655263+iwataro@users.noreply.github.com>
2022-03-14 22:10:15 +0900

    Delete trial1 directory

--- trial1/_assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$('.faq__block__question').on('click', function(){
-	$(this).next().slideToggle();
-
-	if($(this).hasClass("open")) {
-		$(this).removeClass("open");
-	} else {
-		$(this).addClass("open");
-	}
-});
